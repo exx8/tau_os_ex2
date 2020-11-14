8e7c4f7f6dfce7254b823b493063e4fc9cbb5350
exx8 <exx8eran@gmail.com>
2020-11-14 16:56:44 +0200

    .
    Signed-off-by: exx8 <exx8eran@gmail.com>

---
+++ cmake-build-debug/CMakeFiles/tau_os_ex2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tau_os_ex2.dir/main.c.o"
+  "tau_os_ex2"
+  "tau_os_ex2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tau_os_ex2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/tau_os_ex2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for tau_os_ex2.
+# This may be replaced when dependencies are built.
